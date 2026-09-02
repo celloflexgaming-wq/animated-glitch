690dc326c2e5b45294196d1a8101c633bb83e9a2
celloflexgaming-wq <celloflexgaming@gmail.com>
2026-09-02 15:10:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Animated Glitch",
+    page_icon="🎞️",
+)
+
+st.title("🎞️ Animated Glitch")
+st.write("De Streamlit-app werkt!")
+
+st.success("✅ Deployment is succesvol gestart.")
